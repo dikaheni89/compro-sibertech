0c93a0a5276776d82d05794972a2038b55706a44
dikaheni89 <dika.ter89@gmail.com>
2023-10-31 14:25:06 +0700

    update staff

--- app/Views/staff/index.php
+++ app/Views/staff/index.php
@@ -1,70 +1,33 @@
-<main id="main">
+<div class="theme-inner-banner">
+  <div class="container">
+    <h2 class="intro-title text-center"><?= $title ?></h2>
+    <ul class="page-breadcrumb style-none d-flex justify-content-center">
+      <li><a href="<?= base_url() ?>">Home</a></li>
+      <li class="current-page"><?= $title ?></li>
+    </ul>
+  </div>
+</div>
 
-  <!-- ======= Breadcrumbs Section ======= -->
-  <section class="breadcrumbs">
-    <div class="container">
-
-      <div class="d-flex justify-content-between align-items-center">
-        <h2><?= $title ?></h2>
-        <ol>
-          <li><a href="<?= base_url() ?>">Home</a></li>
-          <li><?= $title ?></li>
-        </ol>
-      </div>
-
-    </div>
-  </section><!-- End Breadcrumbs Section -->
-
-  <!-- ======= Doctors Section ======= -->
-    <section id="doctors" class="doctors section-bg">
-      <div class="container" data-aos="fade-up">
-
-
-
-          <div class="card">
-            <div class="card-header">
-              <h2><?= $title ?></h2>
+<div class="team-section-two mt-110 mb-160 lg-mt-60 lg-mb-100">
+  <div class="container">
+    <div class="row">
+      <?php foreach ($staff as $staff) { ?>
+        <div class="col-xl-3 col-lg-4 col-sm-6" data-aos="fade-up">
+          <div class="team-block-one tran3s mt-50">
+            <div class="img-meta">
+              <?php if ($staff['gambar'] === '') {  ?>
+                <img src="<?= icon() ?>" class="w-100" alt="<?= $staff['nama'] ?>">
+              <?php } else { ?>
+                <img src="<?= base_url('assets/upload/staff/' . $staff['gambar']) ?>" class="w-100" alt="<?= $staff['nama'] ?>">
+              <?php } ?>
             </div>
-            <div class="card-body">
-                      <div class="row justify-content-center">
-
-          <?php foreach ($kategori as $kategori) {
-    $id_kategori_staff = $kategori['id_kategori_staff'];
-    $staff             = $m_staff->kategori_staff($id_kategori_staff);
-
-    if ($staff) {
-        ?>
-            <h3 class="text-center"><?= $kategori['nama_kategori_staff'] ?></h3>
-            <hr>
-            <br>
-            <?php foreach ($staff as $staff) { ?>
-                <div class="col-lg-3 col-md-6">
-                  <div class="member" data-aos="fade-up" data-aos-delay="100">
-                    <div class="member-img">
-                      <?php if ($staff['gambar'] === '') {  ?>
-                        <img src="<?= icon() ?>" class="img-fluid" alt="<?= $staff['nama'] ?>">
-                      <?php } else { ?>
-                        <img src="<?= base_url('assets/upload/staff/' . $staff['gambar']) ?>" class="img-fluid" alt="<?= $staff['nama'] ?>">
-                      <?php } ?>
-                    </div>
-                    <div class="member-info">
-                      <h4><?= $staff['nama'] ?></h4>
-                      <span><?= $staff['jabatan'] ?></span>
-                    </div>
-                  </div>
-                </div>
-                <?php
-              }
-    }
-}
-          ?>
-
-          </div>
-          </div>
+            <a href="team-details.html" class="team-info">
+              <span class="designation-text"><?= $staff['jabatan'] ?></span>
+              <span class="name tran3s"><?= $staff['nama'] ?></span>
+            </a>
+          </div> <!-- /.team-block-one -->
         </div>
-
-      </div>
-    </section><!-- End Doctors Section -->
-
-
-</main><!-- End #main -->
+      <?php } ?>
+    </div>
+  </div>
+</div>
